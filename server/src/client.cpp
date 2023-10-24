0ae43931be77073ca3b11360f7c39ac9e8b16760
Josh-Kwong-HKUST <wfkwong@connect.ust.hk>
2023-10-24 22:58:29 +0800

    finish 1 client implementation

--- server/src/client.cpp
+++ server/src/client.cpp
@@ -1,6 +1,6 @@
 #include "../include/ClientSocket.h"
 
 int main(int argc, char** argv){
-    ClientSocket client(argv[1], atoi(argv[2]));
+    ClientSocket client = ClientSocket(argv[1], atoi(argv[2])); // 47.243.244.116:8888
     client.connectToServer();
 }
