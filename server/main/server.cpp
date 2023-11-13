a6b4f01aa77ff696c3d54fadd84018a188d3bb17
Josh-Kwong-HKUST <wfkwong@connect.ust.hk>
2023-11-13 16:09:40 +0800

    delete redundant

--- server/main/server.cpp
+++ server/main/server.cpp
@@ -12,50 +12,6 @@
 #define PASS_FIELD "userPasswd"
 #define FIELD_TUPLE "(userName, userPasswd)"
 
-// void forwardMessage(int cli_fd, char buffer[BUFFER_SIZE + 7]){
-//     Message* msg = new Message(buffer);
-//     int toId = msg->getToId();
-//     /*
-//         999 means broadcast
-//         server broadcast the message to all clients
-//     */
-//     if (toId == 999){
-//         for (auto client : mapIdToClient){
-//             int targetFd = client.second->getSocket()->getSockfd();
-//             if (targetFd == cli_fd) continue;
-//             printf("Broadcasting message from %d to %d\n", msg->getFromId(), client.first);
-//             printf("Content: %s\n", msg->getContent());
-//             if(send(targetFd, buffer, BUFFER_SIZE + 7, 0) == -1)
-//                 printf("=====Error: Failed to send message to client %d! Errno: %s=====\n", targetFd, strerror(errno));
-//         }
-//     }
-//     else if (toId == 998){   // 998 means this is a request to server, server needs to respond the online list
-//         string onlineList = "Current online: ";
-//         for (auto client : mapIdToClient){
-//             onlineList += to_string(client.first) + " ";
-//         }
-//         onlineList += "\0";
-//         cout << onlineList.c_str() << "\n";
-//         Client* fromClient = mapIdToClient[msg->getFromId()];
-//         Message* msg_ = new Message(998, fromClient->getAccountId());
-//         char* content = msg_->getContent();
-//         memcpy(content, onlineList.c_str(), onlineList.length());
-//         char buffer[BUFFER_SIZE + 7];
-//         msg_->serializeMessage(buffer);
-//         send(fromClient->getSocket()->getSockfd(), buffer, BUFFER_SIZE + 7, 0);
-//         delete msg_;
-//     }
-//     else if(toId >= 100 && toId < 998){ //private chat
-//         if (mapIdToClient.find(toId) == mapIdToClient.end()){
-//             printf("-----System message: Forwarding target id = %d not found!-----\n", toId);
-//             return;
-//         }
-//         Client* toClient = mapIdToClient[toId];
-//         int targetFd = toClient->getSocket()->getSockfd();
-//         send(targetFd, buffer, BUFFER_SIZE + 7, 0);
-//     }
-//     delete msg;
-// }
 const char* loginOrRegisterRequestHandler(const char* request){
     auto tokens = splitString(request, ' ');
     auto requestType = tokens[0];
